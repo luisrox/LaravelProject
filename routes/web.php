91d1e0fb27308cb098ad9392dc7dcc555aef335a
luisr <luisrox.jimenez@gmail.com>
2024-01-16 15:04:56 -0600

    primeras configuraciones con rutas y plantillas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', HomeController::class);
+
+Route::controller(CursoController::class)->group(function(){
+    Route::get('cursos', 'index');
+    Route::get('cursos/create', 'create');
+    Route::get('cursos/{curso}', 'show');
 });
+
+/* Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+
+    return (!empty($categoria) ? "Bienvenido al $curso de la categoría $categoria" : "Bienvenido al curso: $curso");
+}); */

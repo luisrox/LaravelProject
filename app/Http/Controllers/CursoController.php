602a0529548dd09a3e7a8d17e8b7218180cd9fb9
luisr <luisrox.jimenez@gmail.com>
2024-01-17 12:05:05 -0600

    vistas y plantillas blade

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -2,22 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Curso;
 use Illuminate\Http\Request;
 
 class CursoController extends Controller
 {
     public function index(){
-        return view('cursos/index');
+        $cursos = Curso::paginate();
+
+        return view('cursos/index', compact('cursos'));
     }
 
     public function create(){
         return view('cursos/create');
     }
 
-    public function show($curso){
-
-        // compact('curso'); // ['curso' => $curso]
-
+    public function show($id){
+        $curso = Curso::find($id)->first();
         return view('cursos/show', compact('curso'));
     }
 }
